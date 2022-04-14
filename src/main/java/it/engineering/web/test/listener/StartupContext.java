6687c1690602db9072f1c76b6bf636761ce90fd8
Tomislav Tadic <tomislav.tadic.89@gmail.com>
2022-04-14 13:02:45 +0200

    Add check who is loggedin

---
+++ src/main/java/it/engineering/web/test/listener/StartupContext.java
@@ -0,0 +1,41 @@
+package it.engineering.web.test.listener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import it.engineering.web.test.domain.User;
+
+/**
+ * Application Lifecycle Listener implementation class StartupContext
+ *
+ */
+public class StartupContext implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public StartupContext() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	ServletContext servletContext=sce.getServletContext();
+    	List<User> users = new ArrayList<>();
+    	servletContext.setAttribute("loggedUsers", users);
+    }
+	
+}
