c8e16e5410ee720d55e23b751696d609bd99f655
Tomislav Tadic <tomislav.tadic.89@gmail.com>
2022-04-19 18:14:23 +0200

    Add verificator

---
+++ src/main/java/it/engineering/web/test/verificator/Verificator.java
@@ -0,0 +1,25 @@
+package it.engineering.web.test.verificator;
+
+public class Verificator {
+
+	public static boolean isAllDigitString(String data) {
+		for (char c : data.toCharArray()) {
+			if (!Character.isDigit(c)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static boolean isLength(String data, int length) {
+		return data.length() == length;
+	}
+
+	public static boolean isMaxLength(String data, int length) {
+		return data.length() <= length;
+	}
+
+	public static boolean isEmpty(String data) {
+		return data.length() == 0;
+	}
+}
